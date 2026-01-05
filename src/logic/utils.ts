93c01970b84f16954814dad46991d6b3ec789151
Mauro Cicerchia <maurocicerchia98@gmail.com>
2026-01-05 18:01:31 -0300

    fix: use promise all to get all config values for db connection

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -11,11 +11,13 @@ export async function getDbConnectionString({
 }: Pick<AppComponents, 'config'> & { prefix?: string }): Promise<string> {
   let databaseUrl: string | undefined = await config.getString(`${prefix}PG_COMPONENT_PSQL_CONNECTION_STRING`)
   if (!databaseUrl) {
-    const dbUser = await config.requireString(`${prefix}PG_COMPONENT_PSQL_USER`)
-    const dbDatabaseName = await config.requireString(`${prefix}PG_COMPONENT_PSQL_DATABASE`)
-    const dbPort = await config.requireString(`${prefix}PG_COMPONENT_PSQL_PORT`)
-    const dbHost = await config.requireString(`${prefix}PG_COMPONENT_PSQL_HOST`)
-    const dbPassword = await config.requireString(`${prefix}PG_COMPONENT_PSQL_PASSWORD`)
+    const [dbUser, dbDatabaseName, dbPort, dbHost, dbPassword] = await Promise.all([
+      config.requireString(`${prefix}PG_COMPONENT_PSQL_USER`),
+      config.requireString(`${prefix}PG_COMPONENT_PSQL_DATABASE`),
+      config.requireString(`${prefix}PG_COMPONENT_PSQL_PORT`),
+      config.requireString(`${prefix}PG_COMPONENT_PSQL_HOST`),
+      config.requireString(`${prefix}PG_COMPONENT_PSQL_PASSWORD`)
+    ])
     databaseUrl = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabaseName}`
   }
 
