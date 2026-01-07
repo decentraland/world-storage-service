828f169d9c70deb8dddf285b27821b3058fbf127
Mauro Cicerchia <maurocicerchia98@gmail.com>
2026-01-07 16:22:12 -0300

    feat: implement signed fetch middleware

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from '@well-known-components/http-server'
+import { wellKnownComponents } from '@dcl/platform-crypto-middleware'
 import { errorHandler } from '@dcl/platform-server-commons'
 import { deleteWorldStorageHandler } from './handlers/delete-world-storage'
 import { getWorldStorageHandler } from './handlers/get-world-storage'
@@ -7,10 +8,27 @@ import { worldNameMiddleware } from './middlewares/world-name-middleware'
 import type { GlobalContext } from '../types'
 
 // We return the entire router because it will be easier to test than a whole server
-export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContext>> {
+export async function setupRouter(context: GlobalContext): Promise<Router<GlobalContext>> {
+  const { fetcher } = context.components
   const router = new Router<GlobalContext>()
 
   router.use(errorHandler)
+
+  const signedFetchMiddleware = ({ optional = false }: { optional?: boolean } = {}) =>
+    wellKnownComponents({
+      fetcher,
+      optional,
+      onError: (err: Error) => ({
+        error: err.message,
+        message: 'This endpoint requires a signed fetch request. See ADR-44.'
+      }),
+      metadataValidator: (metadata) => {
+        console.log('metadata', metadata)
+        return true
+      } // TODO: Add metadata validator to check for world name and authoritative server origin
+    })
+
+  router.use(signedFetchMiddleware())
   router.use(worldNameMiddleware)
 
   router.get('/storage/world/:key', getWorldStorageHandler)
