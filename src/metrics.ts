548623293e4cd3b778dc99a09bf12f4f40880bbe
Mauro Cicerchia <maurocicerchia98@gmail.com>
2026-01-07 17:15:24 -0300

    fix: remove unused metric

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,16 +1,10 @@
 import { getDefaultHttpMetrics } from '@well-known-components/http-server'
-import { IMetricsComponent } from '@well-known-components/interfaces'
 import { metricDeclarations as logsMetricsDeclarations } from '@well-known-components/logger'
 import { validateMetricsDeclaration } from '@well-known-components/metrics'
 
 export const metricDeclarations = {
   ...getDefaultHttpMetrics(),
-  ...logsMetricsDeclarations,
-  get_world_storage_counter: {
-    help: 'Count calls to get world storage',
-    type: IMetricsComponent.CounterType,
-    labelNames: ['pathname']
-  }
+  ...logsMetricsDeclarations
 }
 
 // type assertions
