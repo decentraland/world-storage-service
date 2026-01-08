5d86de335da3f75cfb2427b740df3b58d37ca3ec
Mauro Cicerchia <maurocicerchia98@gmail.com>
2026-01-08 18:01:55 -0300

    fix: use InvalidRequestError from @dcl/http-commons

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,16 +1,13 @@
+import { InvalidRequestError } from '@dcl/http-commons'
+
+export { InvalidRequestError }
+
 export function isErrorWithMessage(error: unknown): error is Error {
   return error !== undefined && error !== null && typeof error === 'object' && 'message' in error
 }
 
 export function errorMessageOrDefault(error: unknown, defaultMessage = 'Unknown error'): string {
   return isErrorWithMessage(error) ? error.message : defaultMessage
-}
-
-export class InvalidRequestError extends Error {
-  constructor(message: string) {
-    super(message)
-    this.name = 'InvalidRequestError'
-  }
 }
 
 export function isInvalidRequestError(error: unknown): error is InvalidRequestError {
